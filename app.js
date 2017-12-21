15e799b2f2867aa4e6b6f5d8c9068f3a500d29c4
Gustavo Feller <fellergustavo@gmail.com>
2017-12-21 19:44:11 -0200

    Changing this repo to heroku backend

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-//var mongoose = require('mongoose');
+
+var routes = require('./routes');
+var ensemble = require('./routes/ensemble');
+
+
+var app = express();
+
 var mongojs = require('mongojs');
-//var Schema = mongoose.Schema;
-var connectionString = 'iris';
 
+var connectionString;
+var isLocal = false;
+if(isLocal) {
+    connectionString = 'ensembleDataSets';
+}
+else {
+    connectionString = "mongodb://admin:admin@ensemble-shard-00-00-38uq3.mongodb.net:27017,ensemble-shard-00-01-38uq3.mongodb.net:27017,ensemble-shard-00-02-38uq3.mongodb.net:27017/ensembleDataSets?ssl=true&replicaSet=Ensemble-shard-0&authSource=admin"
+}
 
-// Connect to a collection in the database
-var db = mongojs(connectionString, ['irisDetails']);
+var db = mongojs(connectionString);
 
-console.log(db);
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 db.on('error', function (err) {
     console.log('database error', err);
@@ -24,60 +36,70 @@ db.on('connect', function () {
     console.log('database connected');
 })
 
-/*db.irisDetails.find(function (err, docs) {
-  console.log(docs);
-})*/
+var router = express.Router();
 
-
-
-// connect to MongoDB
-//mongoose.connect('mongodb://localhost:27017/iris');
-
-/*var irisSchema = new Schema({
-    "petal width": Number,
-    "petal length": Number,
-    "sepal width": Number,
-    "sepal length": Number,
-    "species": String
-});
-
-var Iris = mongoose.model('Iris', irisSchema);*/
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 4000;
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
+//app.use('/', index);
+//app.use('/users', users);
 
-// code to fix the permission issue in Chrome and other browsers
-app.all('/*', function(req, res, next) {
+
+
+// development only
+/*if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}*/
+
+
+
+router.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
     res.header("Access-Control-Allow-Methods", "GET, POST","PUT");
     next();
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    db.irisDetails.find({}, {'_id': false}, function (err, docs) {
-        res.json(docs);
-    })
-    //res.json({ message: 'hooray! welcome to our api!' });
-});
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+router.get('/getVariablesEnsemble/:ensembleId', ensemble.getVariablesEnsemble(db));
+router.get('/getAllVariables', ensemble.getAllVariables(db));
+router.get('/getAllEnsembles', ensemble.getAllEnsembles(db));
+router.get('/getTemporalVarData/:xIdx/:yIdx/:zIdx/:simulationId/:varId/:ensembleId', ensemble.getTemporalVarData(db));
+router.get('/getMultivariateData/:xIdx/:yIdx/:zIdx/:time/:simulationId/:varIdList', ensemble.getMultivariateVarData(db));
+//router.get('/getMultivariateData', ensemble.getMultivariateVarData(db))
 
 
+//app.use('/geochemEnsemble', router);
+// error handler
+/*app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});*/
+
+// APPLICATION  !!!  ---------------------------------------------------------------------------------------------------
+router.get('/', router);
+// ---------------------------------------------------------------------------------------------------------------------
+
+// catch 404 and forward to error handler
+/*app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});*/
+
+//module.exports = app;
 // START THE SERVER
 // =============================================================================
 app.listen(port);
---
+++ routes/ensemble.js
@@ -0,0 +1,123 @@
+/**
+ * Created by gustavo on 02/05/2017.
+ */
+
+var mongojs = require('mongojs');
+
+var ensembleCollectionString = 'ensemble';
+var variableCollectionString = 'variable';
+var simDataCollectionString = 'simulationData';
+
+exports.getVariablesEnsemble = function (db) {
+    return function (req, res) {
+        var ensembleId = req.params.ensembleId;
+        console.log("ensembleId = "+ensembleId);
+        db.collection(variableCollectionString).find({ensembleId: mongojs.ObjectID(ensembleId)}, function (err, docs) {
+            res.json(docs);
+        })
+    }
+}
+
+exports.getAllEnsembles = function (db) {
+    return function (req, res) {
+        db.collection(ensembleCollectionString).find({}, function (err, docs) {
+            res.json(docs)
+        })
+    }
+}
+
+exports.getAllVariables = function (db) {
+    return function (req, res) {
+        db.collection(variableCollectionString).find({}, function (err, docs) {
+            res.json(docs);
+        })
+    }
+}
+
+exports.getTemporalVarData = function (db) {
+    return function (req, res) {
+        var xIdx = parseInt(req.params.xIdx);
+        var yIdx = parseInt(req.params.yIdx);
+        var zIdx = parseInt(req.params.zIdx);
+        var simulationId = req.params.simulationId;
+        var varId = mongojs.ObjectID(req.params.varId);
+        var ensembleId = req.params.ensembleId;
+        //console.log("ensembleId = "+ensembleId);
+        // First find the variableId and then find the data
+        db.collection(simDataCollectionString).aggregate([
+            { $match: {
+                simulationId: simulationId,
+                "cell.xIdx": xIdx,
+                "cell.yIdx": yIdx,
+                "cell.zIdx": zIdx,
+            }},
+            { $project: {
+                variables: {$filter: {
+                    input: '$variables',
+                    as: 'variable',
+                    cond: {$eq: ['$$variable.variableId', varId]}
+                }},
+                time: 1,
+                simulationId: 1,
+                _id: 0
+            }}
+        ], function (err, docs) {
+            console.log(err);
+            res.json(docs);
+        });
+    };
+        /*db.collection(simDataCollectionString).find({}, function (err, docs) {
+
+        });*/
+}
+
+exports.getMultivariateVarData = function (db) {
+    return function (req, res) {
+        //var body = req.varIdList;
+        //console.log("aeHOOOOOOOOOOOOO");
+        //console.log(req.params.varIdList.split(','));
+        var xIdx = parseInt(req.params.xIdx);
+        var yIdx = parseInt(req.params.yIdx);
+        var zIdx = parseInt(req.params.zIdx);
+        var time = parseFloat(req.params.time);
+        var simulationId = req.params.simulationId;
+        var varIdList = req.params.varIdList.split(',');
+        for(var i = 0; i < varIdList.length; i++) {
+            varIdList[i] = mongojs.ObjectID(varIdList[i]);
+        }
+        console.log("blablabla");
+        console.log(varIdList);
+
+        db.collection(simDataCollectionString).aggregate([
+            { $match: {
+                simulationId: simulationId,
+                "cell.xIdx": xIdx,
+                "cell.yIdx": yIdx,
+                "cell.zIdx": zIdx,
+                time: time
+            }},
+            { $project: {
+                variables: {$filter: {
+                    input: '$variables',
+                    as: 'variable',
+                    cond: {$in: ['$$variable.variableId', varIdList]}
+                }},
+                time: 1,
+                simulationId: 1,
+                _id: 0
+            }}
+        ], function (err, docs) {
+            console.log(err);
+            res.json(docs);
+        });
+        //res.json([]);
+    };
+}
+
+/*exports.getSimDataFromSimulation = function (db) {
+    return function (req, res) {
+        var simName = req.params.simName;
+
+
+    }
+}*/
