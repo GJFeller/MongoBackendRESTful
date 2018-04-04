4a819b9c38b6554f4d6fdd8e8f96ce18f683a6f2
gjfeller <fellergustavo@gmail.com>
2018-04-03 22:10:36 -0300

    Started to implement the interpolation of timesteps

--- routes/ensemble.js
+++ routes/ensemble.js
@@ -3,6 +3,7 @@
  */
 
 var mongojs = require('mongojs');
+var spline = require('cubic-spline');
 
 var ensembleCollectionString = 'ensemble';
 var variableCollectionString = 'variable';
@@ -117,12 +118,88 @@ exports.getMultivariateVarData = function (db) {
         var time = parseFloat(req.params.time);
         var simulationId = req.params.simulationId;
         var varIdList = req.params.varIdList.split(',');
+        var varStringList = varIdList.slice();
         for(var i = 0; i < varIdList.length; i++) {
             varIdList[i] = mongojs.ObjectID(varIdList[i]);
         }
         console.log("blablabla");
         console.log(varIdList);
 
+        /**
+         * Return all timesteps in the following format:
+         * {
+         *      time: float
+         *      simulationId: string
+         *      cell: Object<{
+         *                      xIdx: int
+         *                      yIdx: int
+         *                      zIdx: int
+         *                      x: float
+         *                      y: float
+         *                      z: float
+         *                   }>
+         *      variables: Array<{
+         *                          variableId: ObjectId
+         *                          value: float
+         *                      }>
+         * }
+         * 
+         */
+        db.collection(simDataCollectionString).aggregate([
+            { $match: {
+                simulationId: simulationId,
+                "cell.xIdx": xIdx,
+                "cell.yIdx": yIdx,
+                "cell.zIdx": zIdx,
+            }},
+            { $project: {
+                variables: {$filter: {
+                    input: '$variables',
+                    as: 'variable',
+                    cond: {$in: ['$$variable.variableId', varIdList]}
+                }},
+                time: 1,
+                simulationId: 1,
+                cell: 1,
+                _id: 0
+            }}
+        ], function (err, docs) {
+            console.log(err);
+            docs.sort(function(a,b) { return a.time == b.time ? 0 : +(a.time > b.time) || -1; });
+            var timesteps = docs.map(a => a.time);
+            console.log(timesteps);
+            for(var varIdx = 0; varIdx < varStringList.length; varIdx++) {
+                var varValues = [];
+                docs.forEach(element => {
+                    var varObj = element.variables.filter(function(variableElement){
+                        //console.log(variable.variableId);
+                        return variableElement.variableId == varStringList[varIdx];
+                    });
+                    if(varObj.length > 0) {
+                        varValues.push(varObj[0].value);
+                    }
+                });
+                console.log("varValues");
+                console.log(varValues);
+                if(timesteps.length === varValues.length) {
+                    var splineValue = spline(63, timesteps, varValues);
+                    console.log("Value at time 63 for var="+varStringList[varIdx]+" is "+splineValue);
+                }
+            }
+        });
+
+        /**
+         * Return 1 element in the following format:
+         * {
+         *      time: float
+         *      simulationId: string
+         *      variables: Array<{
+         *                          variableId: ObjectId
+         *                          value: float
+         *                      }>
+         * }
+         * 
+         */
         db.collection(simDataCollectionString).aggregate([
             { $match: {
                 simulationId: simulationId,
